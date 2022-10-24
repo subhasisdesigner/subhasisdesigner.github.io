0256b25c1a212fb82e03c6f01f4cea39b42d42fd
subhasis santra <santrasubhasis15@gmail.com>
2022-10-24 21:36:11 +0530

    file manage

--- src/view/component/materialUI/Button/viewMoreButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import '../Button/allButton.scss';
-export default function ViewMoreButton() {
-    return (
-        <Button className='view_more' size="medium" variant="text" color="primary">View more<span>→</span></Button>
-    );
-}
